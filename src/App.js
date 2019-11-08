f1c3a15aeaf0c15f52ae0e3f8528523ee07f91d9
Leah Cullen <lcullen008@gmail.com>
2019-11-08 14:36:18 +0000

    Static versions completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import EventList from './components/eventList';
+import Form from './components/eventForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  render() {
+    const testPosts = [
+      {
+        id: 1,
+        event: "KPOP World Festival 2019 Ireland",
+        poster: "Leah Cullen",
+        location: "The Helix - DCU",
+        date: "4-July-2019",
+        time: "18:00-22:00",
+        link: "https://www.eventbrite.ie/e/kpop-world-festival-2019-ireland-tickets-63424205506",
+        comments: [],
+        upvotes: 10
+      },
+      {
+        id: 2,
+        event: "Kpop & Khiphop Party x KEvents in Dublin",
+        poster: "Turks Head",
+        location: "Turks Head - Parliament Street Temple Bar",
+        date: "29-Nov-2019",
+        time: "22:00-03:00",
+        link: "https://www.facebook.com/events/396661737605767/",
+        comments: [],
+        upvotes: 12
+      },
+      {
+        id: 3,
+        event: "K Music at London Purcell Room",
+        poster: "Lisa Kim",
+        location: "Purcell Room - London",
+        date: "18/Nov/2019",
+        time: "Not yet disclosed...",
+        link: "https://www.stereoboard.com/k-music-tickets/london",
+        comments: [],
+        upvotes: 15
+      }
+    ];
+    return(
+      <div className = "jumbotron">
+        <div className = "container-fluid">
+          <div className = "row">
+            <div className = "col-md-6 offset-3">
+              <h1>
+                <a href = "/">KPoppers Unite!</a>
+              </h1>
+            </div>
+          </div>
+          <div className = "row">
+            <div className = "col-md-4">
+              <Form />
+            </div>
+            <div className = "col-md-8">
+              <EventList posts = {testPosts}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
