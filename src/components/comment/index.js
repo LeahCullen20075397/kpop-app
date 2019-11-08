b5d098a68d99edbc0a3e5bfea48c3db469078c94
Leah Cullen <lcullen008@gmail.com>
2019-11-08 16:10:26 +0000

    Comment components implemented

---
+++ src/components/comment/index.js
@@ -0,0 +1,22 @@
+import React, {Component, Fragment} from "react";
+import './comment.css';
+
+export default class Comment extends Component {
+    handleVote = () => {
+        this.props.upvoteHandler(this.props.comment.id);
+    };
+    render() {
+        return(
+            <Fragment>
+                <span className = "ptr" onClick = {this.handleVote}>
+                    <button className="glyphicon glyphicon-thumbs-up btn-md">Like</button> 
+                    {`${this.props.comment.upvotes}`} 
+                </span>
+                <span className = "commenntitem">
+                    {`${this.props.comment.comment}(by${this.props.comment.author})`}
+                </span>
+                <p></p>
+            </Fragment>
+        );
+    }
+}
