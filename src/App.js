cc86e6bfc461c84454ac7580e42725114a60e2c9
Leah Cullen <lcullen008@gmail.com>
2019-11-08 14:59:51 +0000

    Added stub API

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import React, {Component} from 'react';
 import EventList from './components/eventList';
 import Form from './components/eventForm';
+import api from './dataStore/stubAPI';
 
-export default class App extends Component{
+export default class App extends Component {
   render() {
-    const testPosts = [
-      {
-        id: 1,
-        event: "KPOP World Festival 2019 Ireland",
-        poster: "Leah Cullen",
-        location: "The Helix - DCU",
-        date: "4-July-2019",
-        time: "18:00-22:00",
-        link: "https://www.eventbrite.ie/e/kpop-world-festival-2019-ireland-tickets-63424205506",
-        comments: [],
-        upvotes: 10
-      },
-      {
-        id: 2,
-        event: "Kpop & Khiphop Party x KEvents in Dublin",
-        poster: "Turks Head",
-        location: "Turks Head - Parliament Street Temple Bar",
-        date: "29-Nov-2019",
-        time: "22:00-03:00",
-        link: "https://www.facebook.com/events/396661737605767/",
-        comments: [],
-        upvotes: 12
-      },
-      {
-        id: 3,
-        event: "K Music at London Purcell Room",
-        poster: "Lisa Kim",
-        location: "Purcell Room - London",
-        date: "18/Nov/2019",
-        time: "Not yet disclosed...",
-        link: "https://www.stereoboard.com/k-music-tickets/london",
-        comments: [],
-        upvotes: 15
-      }
-    ];
+    let posts = api.getAll();
     return(
       <div className = "jumbotron">
         <div className = "container-fluid">
@@ -51,10 +18,10 @@ export default class App extends Component{
           </div>
           <div className = "row">
             <div className = "col-md-4">
-              <Form />
+              <Form/>
             </div>
             <div className = "col-md-8">
-              <EventList posts = {testPosts}/>
+              <EventList posts = {posts}/>
             </div>
           </div>
         </div>
---
+++ src/dataStore/stubAPI.js
@@ -0,0 +1,101 @@
+import _ from "lodash";
+
+class StubAPI {
+    constructor() {
+        this.posts = [
+        {
+            id: 1,
+            event: "KPOP World Festival 2019 Ireland",
+            poster: "Leah Cullen",
+            location: "The Helix - DCU",
+            date: "4-July-2019",
+            time: "18:00-22:00",
+            link: "https://www.eventbrite.ie/e/kpop-world-festival-2019-ireland-tickets-63424205506",
+            comments: [],
+            upvotes: 10
+            },
+            {
+            id: 2,
+            event: "Kpop & Khiphop Party x KEvents in Dublin",
+            poster: "Turks Head",
+            location: "Turks Head - Parliament Street Temple Bar",
+            date: "29-Nov-2019",
+            time: "22:00-03:00",
+            link: "https://www.facebook.com/events/396661737605767/",
+            comments: [],
+            upvotes: 12
+            },
+            {
+            id: 3,
+            event: "K Music at London Purcell Room",
+            poster: "Lisa Kim",
+            location: "Purcell Room - London",
+            date: "18/Nov/2019",
+            time: "Not yet disclosed...",
+            link: "https://www.stereoboard.com/k-music-tickets/london",
+            comments: [],
+            upvotes: 15
+            }  
+        ];
+    }
+
+    getAll() {
+        return this.posts;
+    }
+
+    add(event, poster, location, date, time, link) {
+        let id = 1;
+        let last = _.last(this.posts);
+        if(last) {
+            id = last.id + 1;
+        }
+        let len = this.posts.length;
+        let newLen = this.posts.push({
+            id,
+            event,
+            poster,
+            location,
+            date,
+            time,
+            link,
+            comments: [],
+            upvotes: 0
+        });
+        return newLen > len;
+    }
+
+    upvote(id) {
+        let index = _.findIndex(this.posts, post => post.id === id);
+        if(index !== -1) {
+            this.posts[index].upvotes += 1;
+            return true;
+        }
+        return false;
+    }
+
+    getPost(id) {
+        let index = _.findIndex(this.posts, post => post.id === id);
+        let result = index !== -1 ? this.posts[index] : null;
+        return result;
+    }
+
+    addComment(postId, c, n) {
+        let post = this.getPost(postId);
+        let id = 1;
+        let last = _.last(post.comments);
+        if(last) {
+            id = last.id + 1;
+        }
+        post.comments.push({id: id, comment: c, poster: n, upvotes: 0});
+    }
+
+    upvoteComment(postId, commentId) {
+        let post = this.getPost(postId);
+        let index = _.findIndex(post.comments, c => c.id === commentId);
+        if(index !== -1) {
+            post.comments[index].upvotes += 1;
+        }
+    }
+}
+
+export default new StubAPI();
